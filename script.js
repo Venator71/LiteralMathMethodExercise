315536efdb83763eb6acaf671777709be4260f93
Venator71 <b.johnhunter71@gmail.com>
2022-03-02 18:03:13 -0600

    First Commit no bonus 6 answered

---
+++ script.js
@@ -0,0 +1,56 @@
+// Q 1a
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+// Q 1b
+console.log(warmHugs.toUpperCase());
+
+// Q 1c
+console.log(warmHugs.replace("like","love"));
+
+// Q 2a
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+// Q 2b
+console.log(beenImpaled.slice(18));
+// console.log(beenImpaled.slice(-18));
+// console.log(beenImpaled.length);
+// console.log(beenImpaled.slice(18, 36));
+
+// Q 3a
+let dotDotDot = "..."
+
+// Q 3b
+let skullBones  = `I don't have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+// Q 4
+console.log(Math.PI);
+
+// Q 5
+
+randomNumber = Math.random()
+randomNumber *= 3;
+console.log(randomNumber);
+
+// Bonus Questions
+
+// 6
+// "Let it go!"= "Let it go!, Let it go!";
+
+// 7a
+let reindeer = "Reindeers are better than people.";
+
+console.log(reindeer.replaceAll(" ","_"));
+
+// 8
+
+console.log(Math.sqrt(2));
+
+// 9
+
+let newRandomNumber = (Math.floor((Math.random() * 16) + 7));
+console.log(newRandomNumber);
+
+
+// console.log(Math.floor((Math.random() * 16) + 7));
